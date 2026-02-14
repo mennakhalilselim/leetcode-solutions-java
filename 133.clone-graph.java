3142aaf7778c602101196876fd7c8c0df44b7967
Menna Selim <mennakhalilselim@gmail.com>
2026-02-14 17:04:10 +0200

    133.clone-graph

---
+++ 133.clone-graph.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=133 lang=java
+ *
+ * [133] Clone Graph
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    HashMap<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null){
+            return null;
+        }
+        
+        if(visited.containsKey(node)){
+            return visited.get(node);
+        }
+
+        Node clonedNode = new Node(node.val);
+        visited.put(node, clonedNode);
+
+        for (Node neighbor : node.neighbors){
+            clonedNode.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return clonedNode;
+    }
+}
+// @lc code=end
+// time O(V+E)
+// space O(V)
+
